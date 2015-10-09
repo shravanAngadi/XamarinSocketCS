6bc254c23ff78054354d44864f5dcbb4bedfe6ee
Airatch <airatch@Airatchs-MacBook-Pro.local>
2015-10-09 18:00:09 +0530

    changes

--- Hello_Mac_CS/Main.cs
+++ Hello_Mac_CS/Main.cs
@@ -14,7 +14,7 @@ namespace Hello_Mac_CS
 	{
 		static void Main (string[] args)
 		{
-
+			NSPasteboard
 			Console.WriteLine("Hello Mac from CS!");
 
 			IPAddress ipAddress =  IPAddress.Parse ("127.0.0.1");
@@ -34,13 +34,14 @@ namespace Hello_Mac_CS
 				{
 					Console.WriteLine("Reading from the scoket......");
 					requestCount = requestCount + 1;
+					Console.WriteLine("Request number : " + requestCount);
 					NetworkStream networkStream = clientSocket.GetStream();
 					byte[] bytesFrom = new byte[10025];
-					networkStream.Read(bytesFrom, 0, (int)clientSocket.ReceiveBufferSize);
+					networkStream.Read(bytesFrom, 0, ((int)clientSocket.ReceiveBufferSize - 1));
 					string dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
 					dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
 					Console.WriteLine(" >> Data from client - " + dataFromClient);
-					string serverResponse = "Last Message from client" + dataFromClient;
+					string serverResponse = "Saying hello to cocoa.";// + dataFromClient;
 					Byte[] sendBytes = Encoding.ASCII.GetBytes(serverResponse);
 					networkStream.Write(sendBytes, 0, sendBytes.Length);
 					networkStream.Flush();
@@ -48,7 +49,15 @@ namespace Hello_Mac_CS
 				}
 				catch (Exception ex)
 				{
-					Console.WriteLine(ex.ToString());
+					Console.WriteLine(ex.ToString() + serverSocket);
+
+					//serverSocket = new TcpListener (ipAddress , 8888);
+					//clientSocket = default(TcpClient);
+					//serverSocket.Start();
+					//Console.WriteLine(" >> Server Started Again");
+					//clientSocket = serverSocket.AcceptTcpClient();
+					//Console.WriteLine(" >> Accept connection from client");
+					//requestCount = 0;
 				}
 			}
 
